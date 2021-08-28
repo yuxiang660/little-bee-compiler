b9f3be12768e1cafb1f443ac594f3f3eafc32d98
yuxiangw <yuxian660@163.com>
2021-08-28 23:20:20 +0800

    rename

--- src/parser/symbol.cpp
+++ src/parser/symbol.cpp
@@ -4,17 +4,17 @@
 
 namespace LBC
 {
-const int no_level = -1;
+const int no_env = -1;
 
 SymbolNode::SymbolNode(const char* name, Type type) :
    m_name(name),
    m_type(type),
-   m_env(no_level)
+   m_env(no_env)
 {}
 
 std::string SymbolNode::to_string() const
 {
-   if (m_env == no_level) return m_name;
+   if (m_env == no_env) return m_name;
    return m_name + "(" + std::to_string(m_env) + ")";
 }
 
