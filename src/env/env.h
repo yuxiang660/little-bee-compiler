79adf76d97216431a0fb1aeec5b9d57f08d47ed6
yuxiangw <yuxian660@163.com>
2021-08-30 00:21:16 +0800

    revert env changes

--- src/env/env.h
+++ src/env/env.h
@@ -11,20 +11,20 @@ namespace LBC
 
 typedef std::unordered_map<TokenPtr, NodePtr> SymbolMap;
 
-class Env
+class Env : public std::enable_shared_from_this<Env>
 {
 public:
    static int s_id;
 
 public:
-   explicit Env(Env* prev_env);
+   explicit Env(std::shared_ptr<Env> prev_env);
    void put(TokenPtr token, NodePtr symbol);
    bool is_redefine(TokenPtr token) const;
    NodePtr get(TokenPtr token) const;
 
 private:
    int m_id;
-   Env* m_prev_env;
+   std::shared_ptr<Env> m_prev_env;
    SymbolMap m_table;
 };
 
